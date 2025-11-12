767f2853ca43cf9bd32412390f354cba66825f3a
SauceCode01 <sauce.code.01@gmail.com>
2025-11-12 22:01:37 +0800

    removed persistent dark mode kasi andaming error awit

--- src/providers/GlobalContextProvider.tsx
+++ src/providers/GlobalContextProvider.tsx
@@ -31,13 +31,13 @@ export const GlobalContextProvider = ({
 }: {
   children: ReactNode;
 }) => {
-  // const [isDarkMode, setIsDarkMode] = useState(localStorage.getItem("darkMode") === "true" || false);
-
-  
   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  const setDarkmodeFunc : Dispatch<SetStateAction<boolean>> = (value) => {
+  // const [isDarkMode, setIsDarkMode] = useState(localStorage? localStorage.getItem("darkMode") === "true" || false : false);
+
+  const setDarkmodeFunc: Dispatch<SetStateAction<boolean>> = (value) => {
     setIsDarkMode(value);
+    // localStorage?.setItem("darkMode", value ? "true" : "false");
     // localStorage.setItem("darkMode", value ? "true" : "false");
   };
 

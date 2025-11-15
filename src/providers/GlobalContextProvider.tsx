7f8c65a4b0c32bcae07606ec6ef52ee2952001ba
SauceCode01 <sauce.code.01@gmail.com>
2025-11-15 15:47:46 +0800

    fix: local storage error

--- src/providers/GlobalContextProvider.tsx
+++ src/providers/GlobalContextProvider.tsx
@@ -31,8 +31,10 @@ export const GlobalContextProvider = ({
 }: {
   children: ReactNode;
 }) => {
-  const [isDarkMode, setIsDarkMode] = useState(
-    localStorage.getItem("darkMode") === "true" || false
+  // const [isDarkMode, setIsDarkMode] = useState(
+  //   localStorage.getItem("darkMode") === "true" || false
+  // );
+  const [isDarkMode, setIsDarkMode] = useState(false
   );
 
   // const [isDarkMode, setIsDarkMode] = useState(localStorage? localStorage.getItem("darkMode") === "true" || false : false);
